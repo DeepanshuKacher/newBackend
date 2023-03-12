6ce233c10805ed9f1aa6212a307ca4fbd296616b
deepanshu <kacher.deepanshu895@gmail.com>
2023-03-12 15:54:34 +0530

    mqtt secure setup

--- src/useFullItems/mqtt/client.ts
+++ src/useFullItems/mqtt/client.ts
@@ -1,11 +1,32 @@
+import { ConfigService } from "@nestjs/config";
+
 import * as mqtt from "mqtt";
-const client = mqtt.connect("mqtt://mqtt.eatrofoods.com", {
-  username: "anku",
-  password: "ankuWork$100%",
+
+const config = new ConfigService();
+
+// const client = mqtt.connect({
+//   username: "anku",
+//   password: "ankuWork$100%",
+//   host: "mqtt.eatrofoods.com",
+//   port: 8883,
+//   protocol: "mqtts",
+//   clientId: "nodejs_backend",
+// });
+const client = mqtt.connect("wss://mqtt.eatrofoods.com:8883", {
+  username: config.get("mqttUsername"),
+  password: config.get("mqttPassword"),
 });
 
 client.on("connect", function () {
   console.log("mqtt connected");
+});
+
+client.on("error", (error) => {
+  console.log("mqtt error: ", error);
+});
+
+client.on("reconnect", () => {
+  console.log("mqtt reconnecting");
 });
 
 export { client as mqttClient };
